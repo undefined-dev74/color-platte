f5f767948c72e9b9e57291b651c9e3d991fb9fa9
JuniorBaymax <cooladitya953@gmail.com>
2020-09-30 23:31:45 +0530

    Styled Colorbox added some transition or effects

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
-import '../style/ColorBox.css';
+import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "../style/ColorBox.css";
+
 class ColorBox extends Component {
-    render() {
-        return (
-            <div style={{background:this.props.background}} className='ColorBox'>
-                <span >{this.props.name}</span>
-                <span>More</span>
+  render() {
+    const { name, background } = this.props;
+    return (
+      <CopyToClipboard>
+        <div style={{ background }} className="ColorBox">
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
             </div>
-        )
-    }
+            <button className="copy-button">Copy</button>
+          </div>
+          <span className="see-more">More</span>
+        </div>
+      </CopyToClipboard>
+    );
+  }
 }
 
 export default ColorBox;
