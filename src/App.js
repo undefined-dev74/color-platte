f8305e3709f220784b55f8b9667f418a60300006
JuniorBaymax <cooladitya953@gmail.com>
2020-09-30 08:31:59 +0530

    Modiefied file structure and initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Ipod from './Container/Ipod/Ipod';
+
+class  App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Ipod/>
+      </div>
+    );
+  }
+}
+
+export default App;
