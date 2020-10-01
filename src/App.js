e2ebd144fa1aa9c664b428d10866cf7cccf0dcb3
JuniorBaymax <cooladitya953@gmail.com>
2020-10-02 00:31:34 +0530

    Added generate Palette Module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Palette from "./components/Palette";
 import seedColors from './assets/seedColors';
+import generatePalette  from "./assets/colorHelpers";
 
 class App extends React.Component {
   render() {
+    console.log(generatePalette(seedColors[4]))
     return (
       <div>
         <Palette {...seedColors[4]} />
